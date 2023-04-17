5bc44e84c272a5608c7b31ec7794411b1f954144
Nazarii <nazariystr@gmail.com>
2023-04-17 18:59:49 +0300

    Toggle button class docs

--- src/GUI/ToggleButton/ToggleButton.cpp
+++ src/GUI/ToggleButton/ToggleButton.cpp
@@ -6,36 +6,47 @@ ToggleButton::ToggleButton(QWidget* parent, int width, int height,
     QCheckBox(parent),
     mBgColor(bgColor), mCircleColor(circleColor), mActiveColor(activeColor)
 {
+    // set size
     setFixedSize(width, height);
+    // set cursor on hover event
     setCursor(Qt::PointingHandCursor);
 
+    // run debug after button state change
     connect(this, &QCheckBox::stateChanged, this, &ToggleButton::debug);
 }
 
 void ToggleButton::paintEvent(QPaintEvent* event)
 {
+    // adjucst painter
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing);
     painter.setPen(Qt::NoPen);
 
+    // rectangle init
     QRect rect(0, 0, this->width(), this->height());
 
+    // check if checked
     if (!this->isChecked()) {
+        // draw background
         painter.setBrush(QColor(mBgColor));
         painter.drawRoundedRect(0, 0, rect.width(), this->height(),
                                 this->height() / 2, this->height() / 2);
 
+        // draw circle
         painter.setBrush(QColor(mCircleColor));
         painter.drawEllipse(3, 3, 22, 22);
     } else {
+        // draw background
         painter.setBrush(QColor(mActiveColor));
         painter.drawRoundedRect(0, 0, rect.width(), this->height(),
                                 this->height() / 2, this->height() / 2);
 
+        // draw circle
         painter.setBrush(QColor(mCircleColor));
         painter.drawEllipse(this->width() - 26, 3, 22, 22);
     }
 
+    // paint end
     painter.end();
 }
 
--- src/GUI/ToggleButton/ToggleButton.h
+++ src/GUI/ToggleButton/ToggleButton.h
@@ -6,24 +6,51 @@
 #include <QPropertyAnimation>
 #include <QString>
 
+/// @brief Custom QCheckBox with a toggle switch-like appearance. This class
+/// inherits from QCheckBox and provides a custom look for the checkbox. The
+/// checkbox is displayed as a circle that moves to the left or right within a
+/// rectangular background to indicate the on or off state. The background,
+/// circle, and active circle colors can be customized.
 class ToggleButton : public QCheckBox
 {
     Q_OBJECT
 
   private:
+    /// @brief Background color of the toggle button.
     QString mBgColor;
+    /// @brief Color of the circle inside the toggle button.
     QString mCircleColor;
+    /// @brief Color of the circle when the toggle button is active.
     QString mActiveColor;
 
+    /// @brief Helper status function for debugging.
     void debug();
 
   public:
+    /// @brief Constructor for ToggleButton.
+    /// @param parent The parent widget.
+    /// @param width The width of the toggle button.
+    /// @param height The height of the toggle button.
+    /// @param bgColor The background color of the toggle button.
+    /// @param circleColor The color of the circle inside the toggle button.
+    /// @param activeColor The color of the circle when the toggle button is
+    /// active.
     ToggleButton(QWidget* parent = nullptr, int width = 60, int height = 28,
                  QString bgColor = "#777", QString circleColor = "#DDD",
                  QString activeColor = "#00A300");
 
   protected:
+    /// @brief Overridden paint event.This function is called when the toggle
+    /// button needs to be repainted. It draws the background and the circle
+    /// inside the toggle button.
+    /// @param event The paint event.
     virtual void paintEvent(QPaintEvent* event) override;
+
+    /// @brief Overridden hit button function. This function is called when the
+    /// user clicks on the toggle button. It checks if the click was inside the
+    /// circle or not, and toggles the state accordingly.
+    /// @param pos The position of the click.
+    /// @return Whether the click was inside the circle or not.
     virtual bool hitButton(const QPoint& pos) const override;
 };
 
