b9fb787c029d32438c05c80ae46978a3047f031b
Nazarii <nazariystr@gmail.com>
2023-05-02 23:11:54 +0300

    Add base UI components to start/restart and stop testing noise reduction.

--- src/GUI/ModelTester/ModelTester.cpp
+++ src/GUI/ModelTester/ModelTester.cpp
@@ -2,9 +2,65 @@
 
 ModelTester::ModelTester(QWidget* parent) : QDialog(parent)
 {
-    mMainLayout = new QVBoxLayout(this);
-    setLayout(mMainLayout);
 
+    mDurationText = new QLabel(this);
+    mTimer = new QTimer(this);
+    mRecordButton = new QPushButton("Start recording", this);
+    mSaveButton = new QPushButton("Save recording", this);
+    mMainLayout = new QVBoxLayout(this);
+
+    mMainLayout->addWidget(mDurationText);
+    mMainLayout->addWidget(mRecordButton);
+    mMainLayout->addWidget(mSaveButton);
+
+    restartDialog();
+
+    setLayout(mMainLayout);
     setFixedSize(300, 100);
     setWindowTitle("Test Noise Reduction:");
+
+    connect(mRecordButton, &QPushButton::clicked, this,
+            &ModelTester::startRecording);
+    connect(mSaveButton, &QPushButton::clicked, this,
+            &ModelTester::stopRecording);
+    connect(mTimer, &QTimer::timeout, this, &ModelTester::updateDuration);
+}
+
+int ModelTester::exec()
+{
+    restartDialog();
+    return QDialog::exec();
+}
+
+void ModelTester::restartDialog()
+{
+    mDuration = 0;
+    mDurationText->setText("Duration: 0 s");
+    mDurationText->hide();
+    mSaveButton->hide();
+    mRecordButton->show();
+}
+
+void ModelTester::startRecording()
+{
+    mDuration = 0;
+    mRecordButton->hide();
+    mDurationText->show();
+    mSaveButton->show();
+
+    mTimer->start(1000);
+}
+
+void ModelTester::stopRecording()
+{
+    // mDurationText->hide();
+    mSaveButton->hide();
+
+    mTimer->stop();
+}
+
+void ModelTester::updateDuration()
+{
+    mDuration++;
+    mDurationText->setText(QString("Duration: %1 s").arg(mDuration));
 }
--- src/GUI/ModelTester/ModelTester.h
+++ src/GUI/ModelTester/ModelTester.h
@@ -3,6 +3,8 @@
 
 #include <QDialog>
 #include <QLabel>
+#include <QPushButton>
+#include <QTimer>
 #include <QVBoxLayout>
 
 #include "../TextLabel/TextLabel.h"
@@ -13,12 +15,28 @@ class ModelTester : public QDialog
     Q_OBJECT
 
   private:
+    int mDuration;
+    QTimer* mTimer;
+
+    QLabel* mDurationText;
+    QPushButton* mRecordButton;
+    QPushButton* mSaveButton;
+
     QVBoxLayout* mMainLayout;
+
+    void restartDialog();
 
   public:
     /// @brief Constructor for ModelTester class.
     /// @param parent A pointer to the QWidget parent. Defaults to nullptr.
     ModelTester(QWidget* parent = nullptr);
+
+    int exec();
+
+  public slots:
+    void startRecording();
+    void updateDuration();
+    void stopRecording();
 };
 
 #endif // MODEL_TESTER_H
